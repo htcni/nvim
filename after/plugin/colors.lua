74f5f84e51eb5e3c94b5ed1341c4cb0d9f2d95d3
Nitin Bartwal <nitinbartwal6@gmail.com>
2024-03-21 10:08:01 +0530

    modified theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,19 +1,19 @@
 require('rose-pine').setup({
-	disable_background = true,
-	styles = {
-        bold = true,
-        italic = false,
-    },
+  disable_background = true,
+  styles = {
+    bold = true,
+    italic = false,
+  },
 
 })
 
 
 function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+  color = color or "rose-pine"
+  vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
